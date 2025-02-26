710ffd4c7752fb3c3534b545623236ccd732420f
sojeongw <sojeongw.official@gmail.com>
2025-02-26 19:08:31 +0900

    메모리 가시성

---
+++ src/thread/sync/BankMain.java
@@ -0,0 +1,27 @@
+package thread.sync;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class BankMain {
+
+    public static void main(String[] args) throws InterruptedException {
+        BankAccount bankAccount = new BankAccountV1(1000);
+
+        Thread t1 = new Thread(new WithdrawTask(bankAccount, 800), "t1");
+        Thread t2 = new Thread(new WithdrawTask(bankAccount, 800), "t1");
+
+        t1.start();
+        t2.start();
+
+        // 검증 완료까지 잠시 대기
+        sleep(500);
+        log("t1 state: " + t1.getState());
+        log("t2 state: " + t2.getState());
+
+        t1.join();
+        t2.join();
+
+        log("최종 잔액: " + bankAccount.getBalance());
+    }
+}

d5183d0b6bdc94e0c1810a240ea08cf15ac5a9a4
sojeongw <sojeongw.official@gmail.com>
2025-02-27 14:09:36 +0900

    동기화: synchronized

--- src/thread/sync/BankMain.java
+++ src/thread/sync/BankMain.java
@@ -6,10 +6,10 @@ import static util.ThreadUtils.sleep;
 public class BankMain {
 
     public static void main(String[] args) throws InterruptedException {
-        BankAccount bankAccount = new BankAccountV1(1000);
+        BankAccount bankAccount = new BankAccountV3(1000);
 
         Thread t1 = new Thread(new WithdrawTask(bankAccount, 800), "t1");
-        Thread t2 = new Thread(new WithdrawTask(bankAccount, 800), "t1");
+        Thread t2 = new Thread(new WithdrawTask(bankAccount, 800), "t2");
 
         t1.start();
         t2.start();

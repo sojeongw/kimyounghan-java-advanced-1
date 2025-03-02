91708e67e03365f88c80ed66df1079e95b12f3a0
sojeongw <sojeongw.official@gmail.com>
2025-03-02 12:18:56 +0900

    동시성 컬렉션

---
+++ src/thread/collection/simple/list/SimpleListMainV2.java
@@ -0,0 +1,43 @@
+package thread.collection.simple.list;
+
+import static util.MyLogger.log;
+
+public class SimpleListMainV2 {
+
+    public static void main(String[] args) throws InterruptedException {
+//        test(new BasicList());
+//        test(new SyncList());
+        test(new SyncProxyList(new BasicList()));
+    }
+
+    private static void test(SimpleList list) throws InterruptedException {
+        log(list.getClass().getSimpleName());
+
+        Runnable runnableA = new Runnable() {
+            @Override
+            public void run() {
+                list.add("A");
+                log("Thread-0: list.add(A)");
+            }
+        };
+
+        Runnable runnableB = new Runnable() {
+            @Override
+            public void run() {
+                list.add("B");
+                log("Thread-1: list.add(B)");
+            }
+        };
+
+        Thread thread1 = new Thread(runnableA);
+        Thread thread2 = new Thread(runnableB);
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        log(list);
+    }
+}
